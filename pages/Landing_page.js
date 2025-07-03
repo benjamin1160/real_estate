51c379cd65327f9b08a1e819cbdab4e4d33b1af5
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-07-03 16:02:58 -0400

    Add Landing_page with blueberry text

---
+++ pages/Landing_page.js
@@ -0,0 +1,9 @@
+import DefaultLayout from "@/features/Layouts/DefaultLayout";
+
+export default function Landing_page() {
+  return (
+    <DefaultLayout>
+      <div>blueberry</div>
+    </DefaultLayout>
+  );
+}
