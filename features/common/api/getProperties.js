b091f769cf90d5cac2f9faf2cdb4e231c1726a41
benjamin1160 <benjamin@mobilehomeguys.ai>
2025-07-05 12:39:38 -0400

    Replicate RapidAPI requests

--- features/common/api/getProperties.js
+++ features/common/api/getProperties.js
@@ -1,7 +1,15 @@
-import { axios } from "@/lib/axios";
+import axios from "axios";
+
+const BASE_URL = "https://bayut.p.rapidapi.com";
+const HEADERS = {
+  "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+  "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+};
 
 export const getProperties = async (num) => {
-  const { data } = await axios.get("/properties/list", {
+  const options = {
+    method: "GET",
+    url: `${BASE_URL}/properties/list`,
     params: {
       locationExternalIDs: "5002,6020",
       purpose: "for-sale",
@@ -10,8 +18,10 @@ export const getProperties = async (num) => {
       sort: "city-level-score",
       hasVideo: true,
       hasFloorPlan: true,
-      hasPanorama: true
-    }
-  });
+      hasPanorama: true,
+    },
+    headers: HEADERS,
+  };
+  const { data } = await axios.request(options);
   return data.hits;
 };
